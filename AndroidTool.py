d1bfd1b8ebe2cec34c71f25cbe771f3c6572842d
Miki00711 <134545118+Miki00711@users.noreply.github.com>
2024-01-26 15:20:38 +0100

    Add files via upload

---
+++ AndroidTool.py
@@ -0,0 +1,30 @@
+import colorsys
+import os
+from pystyle import Colors, Colorate
+import string
+
+logo = """
+  ___      _____           _ 
+ / _ \    |_   _|         | |
+/ /_\ \_ __ | | ___   ___ | |
+|  _  | '_ \| |/ _ \ / _ \| |
+| | | | | | | | (_) | (_) | |
+\_| |_/_| |_\_/\___/ \___/|_|
+                             
+                             """
+
+def main():
+    os.system("cls") 
+    print(Colors.pink + logo)
+    print(Colors.purple + "[" + Colors.white + "1" + Colors.purple + "]" + " " + Colors.white + "discord webhook message sender")
+    print(Colors.purple + "[" + Colors.white + "2" + Colors.purple + "]" + " " + Colors.white + "webcamera")
+    print(Colors.purple + "[" + Colors.white + "3" + Colors.purple + "]" + " " + Colors.white + "servers hacking")
+    print(Colors.purple + "[" + Colors.white + "4" + Colors.purple + "]" + " " + Colors.white + "Shutdown computers with wifi")
+    print(Colors.purple + "[" + Colors.white + "5" + Colors.purple + "]" + " " + Colors.white + "skull")
+    print(Colors.purple + "[" + Colors.white + "6" + Colors.purple + "]" + " " + Colors.white + "IP pinger")
+    print(Colors.purple + "[" + Colors.white + "7" + Colors.purple + "]" + " " + Colors.white + "Wifi Tools |*| SOON")
+    print(Colors.purple + "[" + Colors.white + "8" + Colors.purple + "]" + " " + Colors.white + "IP grabber")
+    input1 = input()
+    
+main()
+input()
